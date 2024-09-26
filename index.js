51a8c5e38c6aa806d1c6e52c448856f64cd95835
jluchett <jluchett@hotmail.com>
2024-09-26 10:21:41 -0500

    Instalado y configurado nodemon

--- index.js
+++ index.js
@@ -9,15 +9,6 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get('/download', (req, res) => {
-  const fileUrl = 'https://drive.google.com/uc?export=download&authuser=0view&id=1haodCRVipkQ1S6p6O8bEGyr--Q-K3P0L';
-  request(fileUrl).pipe(res);
-});
-
-app.get('/api', async (req, res) => {
-  res.json('Hola mundo');
-});
-
 app.use('/api/canciones', cancionesRoutes);
 
 app.use(errorHandler)
